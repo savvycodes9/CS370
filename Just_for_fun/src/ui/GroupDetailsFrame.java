45bc130301e11c8ab0aa429c09a24b179ccc1a40
savvycodes9 <savvysmith04@gmail.com>
2025-11-20 19:22:32 -0800

    all buttons have add action listener for backend connection and verification of user name

--- Just_for_fun/src/ui/GroupDetailsFrame.java
+++ Just_for_fun/src/ui/GroupDetailsFrame.java
@@ -36,39 +36,45 @@ public class GroupDetailsFrame {
         panel.setLayout(null);
 
         groupTitleLabel = new JLabel("Group Title: ");
-        groupTitleLabel.setBounds(150,20,200,25);
+        groupTitleLabel.setBounds(200,20,200,25);
         panel.add(groupTitleLabel);
 
         titleText = new JTextField();
-        titleText.setBounds(230,20,130,25);
+        titleText.setBounds(300,20,130,25);
         panel.add(titleText);
 
         addUserLabel = new JLabel("Add User: ");
-        addUserLabel.setBounds(150,50,200,25);
+        addUserLabel.setBounds(200,50,200,25);
         panel.add(addUserLabel);
 
+
         userText = new JTextField();
-        userText.setBounds(230,50,130,25);
+        userText.setBounds(300,50,130,25);
         panel.add(userText);
 
         JButton saveBtn = new JButton("Save and Close");
         panel.add(saveBtn);
         saveBtn.setBounds(10,20,150,100);
+        saveBtn.addActionListener(e->{
+        // here is where people click save and then it saves to the database
+        });
 
-        saveBtn.setFocusPainted(false);
-        saveBtn.setBorderPainted(false);
-        saveBtn.setContentAreaFilled(true);
-        saveBtn.setOpaque(true);
         saveBtn.setBackground(new Color(0xadd8e6));
-        saveBtn.setForeground(Color.BLACK);
 
         JButton addUserBtn = new JButton("Add");
-        addUserBtn.setBounds(389,50,90,20);
+        addUserBtn.setBounds(450,50,90,20);
         addUserBtn.setFont(new Font("Arial", Font.PLAIN, 10));
+        addUserBtn.addActionListener(e->{
+            //something needs to be added for entering the username and linking it to database and then returning to the this frame and displaying the user name at the bottom
+
+        });
 
         rmvUserBtn = new JButton("Remove user");
-        rmvUserBtn.setBounds(389,100,90,20);
+        rmvUserBtn.setBounds(450,100,90,20);
         rmvUserBtn.setFont(new Font("Arial", Font.PLAIN, 10));
+        rmvUserBtn.addActionListener(e->{
+            //something to verify user is in group and then displays that user being removed successfully
+        });
 
         panel.add(saveBtn);
         panel.add(addUserBtn);
